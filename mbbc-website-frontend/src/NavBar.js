ae0d2a5b7fc80a3c5793440a2d468ea7eb8bf3b3
ctan29 <coelustan7@gmail.com>
2021-05-11 16:30:36 +0800

    Updated NavBar

--- mbbc-website-frontend/src/NavBar.js
+++ mbbc-website-frontend/src/NavBar.js
@@ -5,8 +5,19 @@ const NavBar = () => (
   <nav>
     <ul>
       <li>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
+        <Link to="/" className="navbar-link">
+          <img src="/MbbcLogoNew.png" alt="test" height="55" />
+        </Link>
+      </li>
+      <li>
+        <Link to="/" className="navbar-link">
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link to="/about" className="navbar-link">
+          About
+        </Link>
       </li>
     </ul>
   </nav>
