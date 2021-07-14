8ee9d1e0c5ec3d61cd0b8099dd28c160780eff9d
ctan29 <coelustan7@gmail.com>
2021-07-14 16:45:57 +0800

    Updated home page

--- mbbc-website-frontend/src/pages/HomePage.js
+++ mbbc-website-frontend/src/pages/HomePage.js
@@ -1,8 +1,60 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+/* eslint jsx-a11y/media-has-caption: 0 */
 
 const HomePage = () => (
   <>
-    <h1>Test</h1>
+    <div className="home-video-block">
+      <div className="video-overlay" />
+      <div className="text-overlay">
+        <h1>Do you know your destiny?</h1>
+        <p>
+          To find out your destiny, you are invited to join our church services
+          on Sundays at 9:30 a.m. and 5:00 p.m., in 11-13 Royal St., Kenwick,
+          WA, and Live Streaming on Wednesdays at 7:00 p.m.
+        </p>
+        <p>
+          For our online location, click the &ldquo;Live Preaching&rdquo; button
+          below.
+        </p>
+        <p>
+          Please note the times above are local time (UTC+8) in Perth, WA,
+          Australia.
+        </p>
+        <Link
+          to={{
+            pathname: "https://www.facebook.com/perthmbbc/",
+          }}
+          className="home-button"
+          target="_blank"
+        >
+          LIVE PREACHING
+        </Link>
+        <Link
+          to={{
+            pathname: "https://google.com",
+          }}
+          className="home-button"
+          target="_blank"
+        >
+          BOOKSTORE
+        </Link>
+      </div>
+      <video className="home-video" autoPlay loop muted>
+        <source src="/test-footage.mp4" type="video/mp4" />
+      </video>
+    </div>
+
+    <div className="home-video-block">
+      <div className="home-left-box">
+        <p>test</p>
+      </div>
+      <div className="home-right-box">
+        <p>test2</p>
+      </div>
+    </div>
+
     <p>
       React is a popular JavaScript library for building reusable,
       component-driven user interfaces for web pages or applications.
